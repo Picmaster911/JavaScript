0f636fb4861d4d54f90edea410e4c5dab90544ef
Sharshakov Dmytro <picmaster911@gmail.com>
2023-01-23 19:55:27 +0200

    HW18_39

--- HomeWork18_39/script.js
+++ HomeWork18_39/script.js
@@ -1,28 +1,28 @@
-const buttonR = document.querySelector(`#right`)
-const buttonL = document.querySelector(`#left`)
-const img = document.querySelector(`#myImg`)
-img.src = `/img/3.png`
-let counter = 0;
-buttonR.addEventListener(`click`,()=>{
-    buttonL.style.display =`inline-block`
+const buttonR = document.querySelector(`#right`);
+const buttonL = document.querySelector(`#left`);
+const img = document.querySelector(`#myImg`);
+img.src = `/img/3.png`;
 
-if (counter < 3 ){
-    counter++
-    console.log(counter)
+const f = render (3);
+
+buttonR.addEventListener(`click`,() =>{
+     f(`+`);
+})
+
+buttonL.addEventListener(`click`,() =>{
+    f(`-`);
+})
+
+function render (index) {
+    return function (sing){
+        if (sing === `+`){
+            buttonL.style.display =`inline-block`;
+            index < 5 ? index += 1 : buttonR.style.display =`none`;
+        }
+        if (sing === `-`){
+            buttonR.style.display =`inline-block`;
+            index > 1 ? index -= 1 : buttonL.style.display =`none`;
+        }
+        img.src = `/img/${index}.png`;
+    };
 }
-else  buttonR.style.display =`none`
-
-})
-
-buttonL.addEventListener(`click`,()=>{
-
-    buttonR.style.display =`inline-block`
-    if (counter >= 1 ){
-        counter--
-        console.log(counter)
-    }
-    else {
-        buttonL.style.display =`none`
-    }
-
-})
