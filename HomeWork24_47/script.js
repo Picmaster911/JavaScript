9de5f223759bdafff74ff616802ee405579e0224
Sharshakov Dmytro <picmaster911@gmail.com>
2023-02-17 22:12:51 +0200

    HW22_47chenge

--- HomeWork24_47/script.js
+++ HomeWork24_47/script.js
@@ -1,11 +1,9 @@
 const API = `https://63ee4ddbd466e0c18bad865b.mockapi.io/api/base`
-const  button = document.querySelector(`#submit`);
 const form =   document.querySelector(`#heroesForm`);
-const divCont = document.querySelector(`#mainDiv`);
 const inputName = document.querySelector(`#input1`);
 const  boolFav = document.querySelector(`#fav`);
-const select = document.querySelector(`select`)
-const table =  document.querySelector(`#heroesTable`)
+const select = document.querySelector(`select`);
+const table =  document.querySelector(`#heroesTable`);
 
 async function controller (API,method = "GET",data){
     let  params = {
@@ -17,12 +15,10 @@ async function controller (API,method = "GET",data){
             params.body =  JSON.stringify(data);
         }
         const response = await fetch(API,params);
-        let dataResponse = await response.json();
-        return dataResponse;
+        return  await response.json();
     }
     catch (error){console.log(error)}
 }
-console.log(form);
 
 form.addEventListener(`submit`, async (e) => {
     e.preventDefault();
@@ -30,68 +26,57 @@ form.addEventListener(`submit`, async (e) => {
         name_surname: inputName.value,
         comics: select.value,
     }
-    newItem.favourite = boolFav.checked
+    newItem.favourite = boolFav.checked;
     const data =  await controller (`${API}/heroes`);
-    const heroes =  data.find (item => item.name_surname === newItem.name_surname)
-    if (!heroes) {
+    const sameHero =  data.find (item => item.name_surname === newItem.name_surname);
+    if (!sameHero) {
         const newHero =  await controller (`${API}/heroes`,`POST`,newItem);
        renderHero(newHero);
     }
-
 })
 
 function  renderHero (newHero){
-    console.log(newHero)
-    const divHero =  document.createElement(`div`);
-    const h3Name =  document.createElement(`h3`);
-    const h3Comics = document.createElement(`h3`);
-    const imgdiv =   document.createElement(`div`);
+    let lastElement = 0 ;
+    console.log(newHero);
     const  img = document.createElement(`img`);
-    img.src = `/img/close.png`
-    img.classList.add(`img`)
+    img.src = `/img/close.png`;
+    img.classList.add(`img`);
     const checkFav =   document.createElement(`input`);
-    checkFav.type = `checkbox`
-    const  tr = document.createElement(`tr`)
-
+    checkFav.type = `checkbox`;
+    const  tr = document.createElement(`tr`);
+    const  trButton = document.createElement(`td`);
+    trButton.append(img);
     for (const key in newHero) {
+        lastElement += 1;
         const  td = document.createElement(`td`)
-        if (key === `name_surname`|| key === `comics` ){
-            td.innerText = `${newHero[key]}`
+        if (key === `name_surname`|| key === `comics`){
+            td.innerText = `${newHero[key]}`;
         }
         else if  (key === `favourite`){
-            newHero.favourite === true ? checkFav.checked = true : checkFav.checked = false
-            td.innerText = `Favourite: `
-            td.appendChild(checkFav)
+            newHero.favourite === true ? checkFav.checked = true : checkFav.checked = false;
+            td.innerText = `Favourite: `;
+            td.append(checkFav);
+        }
+        else if (lastElement === 4){
+            td.append(img);
         }
           tr.append(td)
     }
-    const  trButton = document.createElement(`tr`)
-    trButton.append(img)
-    tr.append(trButton)
-    table.append(tr)
-
-
-    // img.src = `/img/close.png`
-    // img.classList.add(`img`)
-    // imgdiv.classList.add(`img_div`)
-    // imgdiv.append(img)
-    // checkFav.type = `checkbox`
-    // newHero.favourite === true ? checkFav.checked = true : checkFav.checked = false
-    // divHero.classList.add(`hero`)
-    // h3Name.innerText = `Имя героя - ${newHero.name_surname}:`
-    // h3Comics.innerText = `Комикс - ${newHero.comics}:`
-    // divHero.append(h3Name)
-    // divHero.append(h3Comics)
-    // divHero.append(checkFav)
-    // divHero.append(imgdiv)
-    // document.body.append(divHero)
+     table.append(tr);
 
     img.addEventListener(`click`,async ()=>{
-        tr.remove()
+        tr.remove();
         await controller (`${API}/heroes/${newHero.id}`,`DELETE`);
-        console.log(`Вы удалили героя из базы `)
+        console.log(`Вы удалили героя из базы ${newHero.name_surname}`);
+    })
+
+    checkFav.addEventListener(`change`,async ()=>{
+        checkFav.checked === true ? newHero.favourite = true: newHero.favourite = false
+        await controller (`${API}/heroes/${newHero.id}`,`PUT`,newHero);
+        console.log(`Change element in base ${newHero.name_surname} , ${newHero.favourite}`)
     })
 }
+
 
 
 
